2e12ae0d06c94086230cef4690bec4df59a04a6a
Victor <victorbarros1130@gmail.com>
2025-09-19 11:01:34 -0700

    feat: Add Open Graph and Twitter Card meta tags to Happy Friday page for improved social media sharing

--- pages/happy-friday/index.js
+++ pages/happy-friday/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import React from "react"
 import { Image, StyleSheet, View } from "react-native"
 import { Footer } from "../../components/footer"
@@ -6,9 +7,30 @@ import { TrackingPixel } from "../../components/trackingPixel"
 
 export default function SextouPage() {
   return (
-    <View style={styles.root}>
-      <TrackingPixel />
-      <NoiseBackground />
+    <>
+      <Head>
+        <title>Happy Friday! 🎉</title>
+        <meta name="description" content="Happy Friday! Enjoy this fun GIF to celebrate the end of the week!" />
+        
+        {/* Open Graph meta tags for social media sharing */}
+        <meta property="og:title" content="Happy Friday! 🎉" />
+        <meta property="og:description" content="Happy Friday! Enjoy this fun GIF to celebrate the end of the week!" />
+        <meta property="og:url" content="https://victorabarros.com/happy-friday" />
+        <meta property="og:image" content="https://media1.tenor.com/m/ONUKiRYfKw0AAAAd/renan-choque-de-cultura.gif" />
+        <meta property="og:image:type" content="image/gif" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="400" />
+        <meta property="og:type" content="website" />
+        
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Happy Friday! 🎉" />
+        <meta name="twitter:description" content="Happy Friday! Enjoy this fun GIF to celebrate the end of the week!" />
+        <meta name="twitter:image" content="https://media1.tenor.com/m/ONUKiRYfKw0AAAAd/renan-choque-de-cultura.gif" />
+      </Head>
+      <View style={styles.root}>
+        <TrackingPixel />
+        <NoiseBackground />
       <Image
         resizeMode="contain"
         source={{
@@ -19,8 +41,9 @@ export default function SextouPage() {
         }}
       />
 
-      <Footer />
-    </View>
+        <Footer />
+      </View>
+    </>
   )
 }
 
